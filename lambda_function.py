c44fa00eb66177ceea059cd4f39e65409aee35e7
manjutrytest <manjunath.gowra@crayon.com>
2025-11-25 14:44:04 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello from Lambda deployed via CodePipeline by manju!",
+            "message": "CodePipeline by manju!",
             "deployed_at": datetime.datetime.utcnow().isoformat() + "Z",
             "event": event
         })
